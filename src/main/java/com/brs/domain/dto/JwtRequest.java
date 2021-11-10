325741a2032e5e28d01963d62493c235d81be205
shyeom1023 <shyeom1023@gmail.com>
2021-11-10 22:07:23 +0900

    jwt 소스

---
+++ src/main/java/com/brs/domain/dto/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.brs.domain.dto;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class JwtRequest implements Serializable {
+
+	private static final long serialVersionUID = 5926468583005150707L;
+	
+	private String username;
+	private String password;
+}
---
+++ src/main/java/com/brs/domain/dto/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.brs.domain.dto;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class JwtResponse implements Serializable {
+
+	private static final long serialVersionUID = -8091879091924046844L;
+	private final String jwttoken;
+
+}
